8fd87bc61fdb60a7a7d12b03f9189cdf83a92369
Sarab <sarabsinghn@gmail.com>
2020-12-31 02:56:49 +0000

    Fianl Upload

---
+++ weatherApp/weatherApp/LocationManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace weatherApp
+{
+    public  class LocationManager
+    {
+
+
+        //https://docs.microsoft.com/en-us/windows/uwp/maps-and-location/get-location
+
+        public async static Task<Geoposition> getPosition() {
+
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+            if(accessStatus != GeolocationAccessStatus.Allowed)
+            {
+                throw new Exception();
+                
+
+            }
+            //Give me whatever you can
+            var geoLocator = new Geolocator { DesiredAccuracyInMeters = 0};
+
+            var position = await geoLocator.GetGeopositionAsync();
+
+            //return the position object 
+            return position;
+
+        }
+
+    }
+}
---
+++ weatherApp/weatherApp/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace weatherApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //test();
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var position = await LocationManager.getPosition();
+
+            RootObject myWeather = await weatherMap.GetWeather(position.Coordinate.Point.Position.Latitude, position.Coordinate.Point.Position.Longitude);
+
+            string icon = String.Format("ms-appx:///Assets/weatherIcons/{0}.png", myWeather.weather[0].icon);
+
+            weatherImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+            // ResultTextblock.Text = myWeather.name + " - " + myWeather.main.temp + " °C  - " + myWeather.weather[0].description;
+
+            ResultTextblock.Text = myWeather.name;
+
+            ResultTextblock1.Text = myWeather.main.temp + " °C ";
+
+            ResultTextblock2.Text = myWeather.weather[0].description;
+
+
+
+
+        }
+
+
+        /*public async  void test(){
+
+            var position = await LocationManager.getPosition();
+
+            RootObject myWeather = await weatherMap.GetWeather(position.Coordinate.Point.Position.Latitude, position.Coordinate.Point.Position.Longitude);
+
+            string icon = String.Format("ms-appx:///Assets/weatherIcons/{0}.png", myWeather.weather[0].icon);
+
+            weatherImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+            // ResultTextblock.Text = myWeather.name + " - " + myWeather.main.temp + " °C  - " + myWeather.weather[0].description;
+
+            ResultTextblock.Text = myWeather.name;
+
+            ResultTextblock1.Text = myWeather.main.temp + " °C ";
+
+            ResultTextblock2.Text = myWeather.weather[0].description;
+
+
+
+        }*/
+
+
+        
+    }
+}
